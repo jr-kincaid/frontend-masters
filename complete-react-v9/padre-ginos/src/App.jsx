0d6d16388142ce9ffe4fb46e5bd2e2fc85dfea7d
J.R. Kincaid <devkincaid@icloud.com>
2025-09-13 12:51:25 -0400

    Finished React Hooks section.

--- complete-react-v9/padre-ginos/src/App.jsx
+++ complete-react-v9/padre-ginos/src/App.jsx
@@ -1,16 +1,22 @@
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
+import Header from "./Header";
+import { CartContext } from "./contexts";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="Pep, cheese, and stuff"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-    </div>
+    <StrictMode>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
+    </StrictMode>
   );
 };
 
